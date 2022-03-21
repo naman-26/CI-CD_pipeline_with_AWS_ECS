6ef4b8efe7d9a94c04a2d670541e0e417a866aef
naman <namanagrawal6222@gmail.com>
2022-03-21 18:03:57 +0530

    added environment variable

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import os
+
+MESSAGE = os.environ.get('MESSAGE')
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return f'{MESSAGE}'
 
 
 app.run(host='0.0.0.0', port=81, debug=True)
